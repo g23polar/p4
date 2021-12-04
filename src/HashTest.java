f5eac3a102018eafad783a84bde33ec31c5738ff
Avi <avimehta9@gmail.com>
2021-12-04 02:12:40 -0500

    hash test

--- src/HashTest.java
+++ src/HashTest.java
@@ -21,13 +21,39 @@ public class HashTest extends TestCase {
     /**
      * Test the hash function
      */
-    public void testh() {
+    public void testH() {
 
         Hash myHash = new Hash(100);
         assertEquals(myHash.h("aaaabbbb", 101), 75);
         assertEquals(myHash.h("aaaabbb", 101), 1640219587 % 101);
     }
     
+    /**
+     * Test the basicInsert function
+     */
+    public void testBasic2() {
+//       Hash myHash2 = new Hash(null);
+//       myHash2.insert("a");
+       //t=new String[11], s = val, c = 10
+       
+    }
+    
+    
+    
+    /**
+     * Tests everything 
+     */
+    public void testInclusive() {
+        assertEquals(0, h.size());
+        assertEquals(5, h.capacity());
+        h.insert("one");
+        h.insert("two");
+        h.insert("three");
+    }
+    
+    /**
+     * Test the basicInsert function
+     */
     public void testBasic() {
         h.insert("one");
         h.insert("two");
